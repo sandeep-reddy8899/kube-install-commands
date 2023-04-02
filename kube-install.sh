ce6d12edd9fb9cedf80933421ffdec0a07b8f96d
sandeep-reddy8899 <127647369+sandeep-reddy8899@users.noreply.github.com>
2023-04-02 08:28:33 +0530

    Update kube-install.sh

--- kube-install.sh
+++ kube-install.sh
@@ -5,44 +5,51 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-mkdir -p /home/ec2-user/eks-cluser-install
-cd /home/ec2-user/eks-cluser-install
+mkdir -p /home/ec2-user/eks-client-install
+cd /home/ec2-user/eks-client-install
 
-
-LOG=eks-cluster-install.log
+LOG=eks-client-install.log
 USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ]; then 
-    echo -e "$R you are not root user, you need rro permissions for this $N"
-    exit 1
-fi 
+if [ $USER_ID -ne 0 ]; then
+	echo  -e "$R You are not the root user, you dont have permissions to run this $N"
+	exit 1
+fi
 
 VALIDATE(){
-    if [ $1 -ne 0 ]; then 
-        echo -e " $2...$R Failed.. $N"
-        exit 1
-    else 
-        echo -e " $2.. $G success..... $N"
-    fi
+	if [ $1 -ne 0 ]; then
+		echo -e "$2 ... $R FAILED $N"
+		exit 1
+	else
+		echo -e "$2 ... $G SUCCESS $N"
+	fi
+
 }
 
 curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" &>> $LOG
-VALIDATE $? "Dowloaded AWS CLI V2"
 
-aws_floder="aws"
-if [ -d "$aws_folder"]; then 
-    echo -e "AWS folder alredy exits ...$Y Skipping unzip $N"
-else 
+VALIDATE $? "Downloaded AWS CLI V2"
+AWS_FOLDER="aws"
+if [ -d "$AWS_FOLDER" ]; then
+    echo -e "AWS directory already exists...$Y SKIPPING Unzip $N"
+else
     unzip awscliv2.zip &>> $LOG
-    VALIDATE "unzip Aws cli v2"
+    VALIDATE "unzip AWS CLI V2"
 fi
 
 ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update &>> $LOG
+
 VALIDATE $? "Updated AWS CLI V2"
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
-VALIDATE $? "Dowloaded eksctl command"
+VALIDATE $? "Downloaded eksctl command"
 chmod +x /tmp/eksctl
-VALIDATE $? "Added excute permissions to eksctl"
+VALIDATE $?  "Added execute permissions to eksctl"
 mv /tmp/eksctl /usr/local/bin
 VALIDATE $? "moved eksctl to bin folder"
-   
+
+curl -s -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.24.10/2023-01-30/bin/linux/amd64/kubectl
+VALIDATE $? "Downloaded kubectl 1.24 version"
+chmod +x kubectl
+VALIDATE $?  "Added execute permissions to kubectl"
+mv kubectl /usr/local/bin/kubectl
+VALIDATE $?  "moved kubectl to bin folder"
