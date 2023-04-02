7b3113629baa7db1831e2717d29928974271ed0d
sandeep-reddy8899 <127647369+sandeep-reddy8899@users.noreply.github.com>
2023-04-02 07:21:26 +0530

    Adding upto cli unziping

---
+++ kube-install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+mkidir -p home/ec2-user/eks-cluser-install
+cd home/ec2-user/eks-cluser-install
+
+
+LOG=eks-cluster-install.log
+USER_ID=$(id -u)
+if [ $USER_ID -ne 0 ]; then 
+    echo -e "$R you are not root user, you need rro permissions for this $N"
+    exit 1
+fi 
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then 
+        echo -e " $2...$R Failed.. $N"
+        exit 1
+    else 
+        echo -e " $2.. $G success..... $N"
+}
+
+curl -s "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" &>>LOG
+VALIDATE $? "Dowloaded AWS CLI V2"
+
+aws_floder="aws"
+if [ -d "$aws_folder"]; then 
+    echo -e "AWS folder alredy exits ...$Y Skipping unzip $N"
+else 
+    unzip awscliv2.zip &>>LOG
+    VALIDATE "unzip Aws cli v2"
+fi
